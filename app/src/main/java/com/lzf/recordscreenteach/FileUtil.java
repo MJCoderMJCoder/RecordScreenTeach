301e69d4776fd9b32c0131025c4cbcdc310c5091
MJCoderMJCoder <598157378@qq.com>
2019-06-11 09:30:25 +0800

    模拟小爱同学的录屏教学训练 类似于按键精灵：实现自动触发点击屏幕

---
+++ app/src/main/java/com/lzf/recordscreenteach/FileUtil.java
@@ -0,0 +1,91 @@
+package com.lzf.recordscreenteach;
+
+import android.content.Context;
+import android.os.Environment;
+import android.os.storage.StorageManager;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+/**
+ * Created by MJCoder on 2019-03-14.
+ */
+
+public class FileUtil {
+
+    /**
+     * 获取新建的文件
+     *
+     * @param context  环境上下文
+     * @param dirName  新建的文件所在的上级目录；可以为空（为空时该文件将直接插入项目的根目录）
+     * @param fileName 新建的文件名：可以为空（为空时返回的时目录）
+     * @return
+     */
+    public static File getFile(Context context, String dirName, String fileName) {
+        File currentFile = null;
+        try {
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                File dir;
+                if (dirName != null) {
+                    dir = new File(Environment.getExternalStorageDirectory(), "AttendanceSystem/" + dirName);
+                } else {
+                    dir = new File(Environment.getExternalStorageDirectory(), "AttendanceSystem");
+                }
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                //                File[] files = dir.listFiles();//获取文件列表
+                //                for (int i = 0; i < files.length; i++) {
+                //                    files[i].delete();//删除该文档下的所有文件
+                //                }
+                if (fileName != null) {
+                    currentFile = new File(dir, fileName);
+                    if (!currentFile.exists()) {
+                        currentFile.createNewFile();
+                    }
+                } else {
+                    currentFile = dir;
+                }
+            } else {
+                String dirTemp = null;
+                StorageManager storageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+                Class<?>[] paramClasses = {};
+                Method getVolumePathsMethod;
+                getVolumePathsMethod = StorageManager.class.getMethod("getVolumePaths", paramClasses);
+                // 在反射调用之前将此对象的 accessible 标志设置为 true，以此来提升反射速度。
+                getVolumePathsMethod.setAccessible(true);
+                Object[] params = {};
+                Object invoke = getVolumePathsMethod.invoke(storageManager, params);
+                for (int i = 0; i < ((String[]) invoke).length; i++) {
+                    if (!((String[]) invoke)[i].equals(Environment.getExternalStorageDirectory().toString())) {
+                        dirTemp = ((String[]) invoke)[i];
+                    }
+                }
+                File dir;
+                if (dirName != null) {
+                    dir = new File(dirTemp, "AttendanceSystem/" + dirName);
+                } else {
+                    dir = new File(dirTemp, "AttendanceSystem");
+                }
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                //                File[] files = dir.listFiles();//获取文件列表
+                //                for (int i = 0; i < files.length; i++) {
+                //                    files[i].delete();//删除该文档下的所有文件
+                //                }
+                if (fileName != null) {
+                    currentFile = new File(dir, fileName);
+                    if (!currentFile.exists()) {
+                        currentFile.createNewFile();
+                    }
+                } else {
+                    currentFile = dir;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return currentFile;
+    }
+}
--- app/src/main/java/com/lzf/recordscreenteach/MainActivity.java
+++ app/src/main/java/com/lzf/recordscreenteach/MainActivity.java
@@ -1,16 +1,26 @@
 package com.lzf.recordscreenteach;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by MJCoder on 2019-06-06.
@@ -36,11 +46,24 @@ public class MainActivity extends Activity {
         });
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onResume() {
         super.onResume();
-        if (new File("/data/data/com.qwh.grapebrowser").exists()) {
-            Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.qwh.grapebrowser");
+        checkPermission(this);
+        MediaProjectionManager mediaProjectionManager = (MediaProjectionManager) this.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        if (mediaProjectionManager != null) {
+            Intent intent = mediaProjectionManager.createScreenCaptureIntent();
+            PackageManager packageManager = this.getPackageManager();
+            if (packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+                //存在录屏授权的Activity
+                this.startActivityForResult(intent, 6003);
+            } else {
+                Toast.makeText(this, "该机型不支持录屏功能", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (new File("/data/data/com.xinhe.sdb").exists()) {
+            Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.xinhe.sdb");
             startActivity(launchIntent);
             //            Intent local = new Intent();
             //            local.setAction("android.intent.action.MAIN");
@@ -60,7 +83,30 @@ public class MainActivity extends Activity {
                 openAccessibilityServiceSettings();
             }
         } else {
-            Toast.makeText(this, "请先安装葡萄浏览器", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "请先安装Fitmind", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * app targetApi 在 6.0 以上时，还需要动态获取权限。
+     *
+     * @param activity
+     */
+    private static void checkPermission(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int checkPermission =
+                    ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO)
+                            + ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE)
+                            + ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            + ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+            if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+                //动态申请
+                ActivityCompat.requestPermissions(activity, new String[]{
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 123);
+            }
         }
     }
 
@@ -74,6 +120,47 @@ public class MainActivity extends Activity {
             Toast.makeText(this, "找 【RecordScreenTeach】 ,然后开启服务", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void setUpMediaRecorder() {
+        String mRecordFilePath = FileUtil.getFile(this, "RecordScreen", null) + File.separator + System.currentTimeMillis() + ".mp4";
+        MediaRecorder mMediaRecorder = new MediaRecorder();
+        //设置音频来源
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        //设置视频来源
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        //输出的录屏文件格式
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        //录屏文件路径
+        mMediaRecorder.setOutputFile(mRecordFilePath);
+        //视频尺寸
+        mMediaRecorder.setVideoSize(ScreenUtils.getScreenW(this), ScreenUtils.getScreenH(this));
+        //音视频编码器
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        //比特率
+        mMediaRecorder.setVideoEncodingBitRate((int) (ScreenUtils.getScreenW(this) * ScreenUtils.getScreenH(this) * 3.6));
+        //视频帧率
+        mMediaRecorder.setVideoFrameRate(20);
+        try {
+            mMediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 6003 && resultCode == Activity.RESULT_OK) {
+            try {
+                setUpMediaRecorder();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "拒绝录屏", Toast.LENGTH_SHORT).show();
         }
     }
 }
